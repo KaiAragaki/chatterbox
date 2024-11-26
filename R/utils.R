c3919daac9b1a5d79b15f3fcb8791db7a777acd3
Kai Aragaki <kaiaragaki@proton.me>
2024-11-26 13:17:05 -0500

    fix: respect user newlines
    Closes #3

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,12 @@
 # If it turns out I need more functionality though I'm ok with taking on a dep
 # Do NOT export this function
 str_wrap <- function(string, width) {
+  strings <- strsplit(string, "\n")
+  sapply(strings, .str_wrap, width = width, simplify = FALSE)[[1]] |>
+    paste0(collapse = "\n")
+}
+
+.str_wrap <- function(string, width) {
   out <- stringi::stri_wrap(
     string,
     width = width, whitespace_only = TRUE, simplify = FALSE
