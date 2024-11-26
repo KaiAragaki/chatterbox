488a4879eb71fe33b9ea5e2c53873c2cb30b4d1e
Kai Aragaki <kaiaragaki@proton.me>
2024-11-26 13:31:07 -0500

    feat: add chat

---
+++ R/chat.R
@@ -0,0 +1,45 @@
+#' Create a figure of a text conversation
+#' @param data Expects a data.frame with columns "message_date", "text",
+#'   "sender_name"
+#' @param me Who should be the person on the right? Should be a person in
+#'   data$sender_name
+#' @param max_char_width How long the message be allowed to get (in characters)
+#'   before wrapping
+#' @param font_size The font size, in points
+#' @param theme Either a list (usually made by make_theme()) or a name of a
+#'   theme
+#' @return An image
+#' @export
+chat <- function(data,
+                 me,
+                 max_char_width = 80,
+                 font_size = 12,
+                 theme = "dark") {
+  if (!is.list(theme)) {
+    theme <- get_theme(theme)
+  }
+  stopifnot(me %in% data$sender_name)
+  data <- data[order(data$message_date), ]
+  # strwidth only deals in inches
+  font_size_in <- font_size / 72.27
+  # Color should depend on service -- iMessage should be blue, SMS should be
+  # green
+  # 'me' will determine who's on the right
+  grid::grid.rect(gp = grid::gpar(fill = theme$bg))
+
+  grobs <- mapply(
+    msgGrob,
+    text = data$text,
+    is_me = data$sender_name == me,
+    MoreArgs = list(theme = theme),
+    SIMPLIFY = FALSE
+  )
+
+  grob_heights <- lapply(grobs, grid::grobHeight)
+  layout <- grid::grid.layout(nrow(data), 1, widths = 1, heights = 1)
+  fg <- grid::frameGrob(layout = layout)
+  for (i in seq_len(nrow(data))) {
+    fg <- grid::placeGrob(fg, grobs[[i]], row = i)
+    grid::grid.draw(fg)
+  }
+}
