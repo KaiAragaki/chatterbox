0b69f6180bc078b5747a83155bb8be105d809a59
Kai Aragaki <kaiaragaki@proton.me>
2024-12-01 14:23:28 -0500

    refactor: create tail differently
    Prior, tails were drawn by creating a circle and 'carving out' regions
    to get a tail. Unfortunately, this 'carving out' was really only
    possible by overlaying other shapes that were the same color as the
    background. This requires matching of the background, which in some
    instances was impossible (particularly if messages were atop one
    another).
    
    This fixes this by creating a custom polygon.

--- R/msg.R
+++ R/msg.R
@@ -16,101 +16,158 @@ msgGrob <- function(text,
                     max_char_width = 80,
                     font_size = 12,
                     theme = "light") {
-  if (!is.list(theme)) {
-    theme <- get_theme(theme)
-  }
-
   label <- str_wrap(text, width = max_char_width)
-  # Dimensions
 
+  # THEMING ----------------------------
+  if (!is.list(theme)) theme <- get_theme(theme)
+  fill_col <- ifelse(is_me, theme$fill_me, theme$fill_you)
+  text_col <- ifelse(is_me, theme$text_me, theme$text_you)
+
+
+  # DIMS -------------------------------
   h_pad <- grid::grobWidth(
     grid::textGrob("MM", gp = grid::gpar(fontsize = font_size))
   )
   v_pad <- grid::grobHeight(
     grid::textGrob("\n", gp = grid::gpar(fontsize = font_size))
   ) * 0.6
-
   label_width <- grid::grobWidth(
     grid::textGrob(label, gp = grid::gpar(fontsize = font_size))
   )
   label_height <- grid::grobHeight(
     grid::textGrob(label, gp = grid::gpar(fontsize = font_size))
   )
-
   xh <- grid::grobWidth(
     grid::textGrob("x", gp = grid::gpar(fontsize = font_size))
-  ) * 0.75
-
-  # Not really dependent on font size - mostly just a pixel thing
-  fudge <- grid::stringWidth(".")
-
-  fill_col <- ifelse(is_me, theme$fill_me, theme$fill_you)
-  text_col <- ifelse(is_me, theme$text_me, theme$text_you)
-  bg <- theme$bg
+  )
+  outer_radius <- xh * 2
+  f <- 0.3
   has_newline <- grepl("\n", label)
-  # FIXME there's gotta be a better way to do this
-  vpname <- paste0(text, round(runif(1), 5))
+  r <- ifelse(has_newline, 1.5, 1.7)
 
-  # Message viewport
-  child_vp <- grid::viewport(
-    name = vpname,
+  # FIXME there's gotta be a better way to do this
+  vpname <- paste0(text, round(stats::runif(1), 5))
+  # Message viewpor
+  msg_vp <- grid::viewport(
+    name = paste0(vpname, ".msgvp"),
     x = ifelse(is_me, 0.95, 0.05),
     y = 0.5,
     width = label_width + h_pad,
     height = label_height + v_pad,
-    just = ifelse(is_me, "right", "left")
+    just = ifelse(is_me, "right", "left"),
+    gp = grid::gpar(col = NA, fill = fill_col)
   )
-  r <- ifelse(has_newline, 1.5, 1.7)
+
   body <- grid::roundrectGrob(
-    vp = vpname,
-    r = r * xh,
-    height = label_height + v_pad,
-    gp = grid::gpar(fill = fill_col, col = NA),
+    r = r * xh, vp = msg_vp, name = paste0(vpname, "body")
   )
-
-  tail_x <- grid::unit(ifelse(is_me, 1, 0), units = "npc")
-  tail <- grid::circleGrob(
-    vp = vpname,
-    x = tail_x, y = xh, r = xh,
-    gp = grid::gpar(fill = fill_col, col = NA)
-  )
-  tail_top_negative <- grid::circleGrob(
-    vp = vpname,
-    x = tail_x + xh / (2 * ifelse(is_me, 1, -1)), y = xh / 2, r = xh / 2,
-    gp = grid::gpar(fill = bg, col = NA)
-  )
-  tail_side_negative <- grid::rectGrob(
-    vp = vpname,
-    x = tail_x + ((xh / 2) + fudge / 2) * ifelse(is_me, 1, -1),
-    y = grid::unit(0.5, units = "npc") + xh / 2,
-    width = xh + fudge, height = 1,
-    gp = grid::gpar(fill = bg, col = NA)
-  )
-
-  # If a message is greater than one line tall, this fills in the 'bald spot'
-  # left due to the rounded corners of the body.
-  #
-  # It's a roundrect because nothing is pixel perfect, and a little rounding
-  # helps hide the seams
-  tail_side_positive <- grid::roundrectGrob(
-    r = grid::unit(0.3, "snpc"),
-    vp = vpname,
-    x = (tail_x) * ifelse(is_me, 1, -1),
-    y = grid::unit(0.5, units = "npc") + xh / 2,
-    just = c(ifelse(is_me, "right", "left"), "top"),
-    width = xh, height = 0.5,
-    gp = grid::gpar(fill = fill_col, col = NA)
-  )
-
   text <- grid::textGrob(
-    vp = vpname,
+    vp = msg_vp,
     label = label, just = "left", x = h_pad / 2,
     gp = grid::gpar(fontsize = font_size, col = text_col)
   )
-
-  msg_list <- grid::gList(
-    body, tail, tail_top_negative, tail_side_negative, tail_side_positive, text
+  tail_vp <- grid::viewport(
+    name = paste0(vpname, ".tailvp"),
+    x = ifelse(is_me, 1, 0),
+    y = outer_radius / 2,
+    width = outer_radius,
+    height = outer_radius,
+    just = ifelse(is_me, c("right", "bottom"), c("left", "bottom"))
   )
 
-  grid::gTree(label = text, children = msg_list, childrenvp = child_vp)
+  tail_points <- tail_path(f)
+  if (is_me) {
+    tail_points$x <- (-tail_points$x + 1 + f)
+  } else {
+    tail_points$x <- tail_points$x - f
+  }
+  tail <- grid::polygonGrob(
+    tail_points$x, tail_points$y,
+    vp = tail_vp, name = paste0(vpname, "tail")
+  )
+  tail_tree <- grid::gTree(
+    vp = msg_vp, childrenvp = tail_vp, children = grid::gList(tail)
+  )
+  grid::gTree(
+    childrenvp = msg_vp, children = grid::gList(tail_tree, body, text)
+  )
+}
+
+# f is the factor the outer radius gets multiplied by to become the inner radius
+# Since the outer radius is 1, f is actually just the inner radius
+#
+# h is the ADDITIONAL height added to the tops of the tail, usually for cases
+# when the message has more than one line
+#
+# n is the number of points the tail should be composed of
+tail_path <- function(f, n = 100, h = 0) {
+  i <- inner_radius(f)
+  o <- inner_radius(1, dir = "out") # outer just special case of inner
+  ie <- inner_edge(f, h)
+  oe <- outer_edge(h)
+
+  all <- list(oe, o, i, ie)
+  # The distribution of points isn't set: if the tail is taller, more should go
+  # to the edges
+  # Thus, the length of each segment needs to be calculated per-tail.
+  total_path_len <- sum(sapply(all, \(x) x$path_len))
+  seg_len <- total_path_len / n
+
+  Reduce(rbind, lapply(all, \(x) x$points(seg_len)))
+}
+
+inner_radius <- function(f, dir = "in") {
+  path_len <- (pi / 2) * f
+  list(
+    points = \(seg_len) {
+      n <- ceiling(path_len / seg_len)
+      # We clip off one at the end in order to chain points together without
+      # duplicates
+
+      # This adjustment keeps the number accurate
+      n <- n + 1
+      theta <- seq(-pi / 2, 0, length.out = n)
+      x <- f * cos(theta)
+      y <- f * sin(theta) + f
+      if (dir == "out") {
+        x <- rev(x)
+        y <- rev(y)
+      }
+      out <- data.frame(x = x, y = y)
+      out[-nrow(out), ]
+    },
+    path_len = path_len
+  )
+}
+
+inner_edge <- function(f, h) {
+  path_len <- 1 - f + h
+  list(
+    points = \(seg_len) {
+      # Inner edge, being the final stroke drawn, does not have its tail clipped
+      n <- ceiling(path_len / seg_len)
+      x <- rep(f, n)
+      y <- seq(f, 1 + h, length.out = n)
+      data.frame(x = x, y = y)
+    },
+    path_len = path_len
+  )
+}
+
+outer_edge <- function(h) {
+  path_len <- h
+  list(
+    points = \(seg_len) {
+      n <- ceiling(path_len / seg_len)
+      x <- rep(1, n)
+      # Stroke direction matters
+      y <- seq(1 + h, 1, length.out = n)
+      data.frame(x = x, y = y)
+    },
+    path_len = path_len
+  )
+}
+
+clip_tail <- function(vec, n = 1) {
+  vec[seq_len(length(vec) - n)]
 }
